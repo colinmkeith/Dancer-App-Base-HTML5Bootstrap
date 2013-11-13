80b609882471f42ccaccb190e5c080486851c3a4
Colin Keith <colinmkeith@gmail.com>
2013-11-13 01:41:14 -0500

    Added login/out pages

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-Dancer-App-Base-HTML5Bootstrap
-==============================
+Dancer::App::Base::HTML5Bootstrap
+=================================
 
-A basic Dancer application written with HTML5/Bootstrap. Clone it to give you a starting point for your own application.
+Dancer::App::Base::HTML5Bootstrap is a base application for [Perl
+Dancer](http://perldancer.org/). The idea is to give Dancer developers a base
+application which takes care of many of the basic features of any web app
+including authentication, a sign-in form and logout page, a basic
+account/dash-board page, a registration page and some core SQL for the accounts.
+
+It use HTML5 so that it is compatible with modern browsers, and uses BootStrap
+for the CSS framework and jQuery for DOM manipulation and AJAX support.
+
--- lib/Dancer/App/Base/HTML5Bootstrap.pm
+++ lib/Dancer/App/Base/HTML5Bootstrap.pm
@@ -1,10 +1,35 @@
 package Dancer::App::Base::HTML5Bootstrap;
+
+# Preamble per http://www.perl.com/pub/2012/04/perlunicook-standard-preamble.html
+use utf8;                          # so literals and identifiers can be in UTF-8
+use v5.16;                         # or later to get "unicode_strings" feature
+use strict;                        # quote strings, declare variables
+use warnings;                      # on by default
+use warnings qw(FATAL utf8);       # fatalize encoding glitches
+use open qw(:std :utf8);           # undeclared streams in UTF-8
+use charnames qw(:full :short);    # unneeded in v5.16
+
 use Dancer ':syntax';
+
+use Dancer::Plugin::Ajax;
+use Dancer::Plugin::Auth::Extensible;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+  template 'index';
 };
 
-true;
+get '/signin' => sub {
+  template 'account/signin';
+};
+
+# 404 Handler:
+any qr{.*} => sub {
+  status 'not_found';
+  template 'error_docs/404', {
+    path => request->path()
+  };
+};
+
+1;
